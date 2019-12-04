7dfa33d607d3fc6e95cfbd86037cd28157538fd8
floresjmjr <floresjmjr@gmail.com>
2019-12-04 10:14:45 -0500

    admin customizations

--- realtors/admin.py
+++ realtors/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Realtor
 
-admin.site.register(Realtor) # Pass the name of the model
+class RealtorAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'hire_date')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin) # Pass the name of the model
