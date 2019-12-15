829d7c17836dce8a68c5fef5e3003ddc7472f06c
floresjmjr <floresjmjr@gmail.com>
2019-12-14 22:37:45 -0500

    commenting send email contact views

--- contacts/views.py
+++ contacts/views.py
@@ -37,13 +37,13 @@ def contact(request):
     contact.save()
 
     # Send email
-    send_mail(
-      'Property Listing Inquiry',
-      'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-      'floresjmjr@gmail.com',
-      [realtor_email, 'floresjmjr@gmail.com'],
-      fail_silently=False
-    )
+    # send_mail(
+    #   'Property Listing Inquiry',
+    #   'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+    #   'floresjmjr@gmail.com',
+    #   [realtor_email, 'floresjmjr@gmail.com'],
+    #   fail_silently=False
+    # )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
 
